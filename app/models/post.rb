e927c8af8e546a2a2a0dd8710e6f4a65a7878abe
siddghosh108 <131663359+siddghosh108@users.noreply.github.com>
2023-12-20 18:51:14 -0800

    Blog-app : Add api #11

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,7 @@ class Post < ApplicationRecord
   attribute :likes_counter, :integer, default: 0
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 },
-                                               allow_nil: true
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_comments
     comments.includes(:author).order(created_at: :desc).limit(3)
